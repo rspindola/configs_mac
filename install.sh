31438f5b74ac469096dc2f81396ade475638949b
Renato <rspindola@live.com>
2025-09-09 08:20:55 -0300

    feat: add new configs

--- install.sh
+++ install.sh
@@ -12,5 +12,7 @@ bash "$DOTFILES_DIR/nvm/install_nvm.sh"
 
 echo "📝 Configuração do VSCode exportada via link:"
 echo "https://vscode.dev/editor/profile/github/0c404552b668348271235e39ab583930?vscode-lang=pt-br"
+echo "https://vscode.dev/profile/github/35e4b6f9aead968dfd04cac439a6f856"
+echo "https://vscode.dev/profile/github/4aac5da2fa1214c28c5791f23b7adb62"
 
 echo "✅ Instalação concluída!"
